17997a17b7b77eaecfd546ba94bf5dd74ff29074
jungduckbae <mazzang8@kakao.com>
2022-01-20 17:31:39 +0900

    view JSP 방식으로 설정 및 공통JS 업로드

--- src/main/java/com/da/common/TestController.java
+++ src/main/java/com/da/common/TestController.java
@@ -1,14 +1,14 @@
 package com.da.common;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class TestController {
 
-  @GetMapping("/")
+  @RequestMapping("/")
   public String index() {
-    return "DealingArt project!!";
+	  return "index";
   }
 }
 
