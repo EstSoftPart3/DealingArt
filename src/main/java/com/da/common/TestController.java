259cdd0acbc48adb0420ae38b35d473b839a8c3a
dudtka37@naver.com <dudtka37@naver.com>
2022-03-14 23:41:29 +0900

    추가 업로드

--- src/main/java/com/da/common/TestController.java
+++ src/main/java/com/da/common/TestController.java
@@ -1,14 +1,22 @@
 package com.da.common;
 
-import org.springframework.stereotype.Controller; 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.da.sample.service.CommonService;
 
 @Controller
 public class TestController {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private CommonService commonService;
 	
 	@RequestMapping("/")
 	public String index() {
@@ -16,11 +24,11 @@ public class TestController {
 		
 		try {
 			
-		String reStr = commonService.encrypt("°æe¢Ø•Ï°Ìi");
+		String reStr = commonService.encrypt("ÍπÄÏòÅÏÇº");
 		
 		String deStr = commonService.decrypt("68160dc64efc04a5a68dce5320b433b3");
 		
-		Map<String, Object> reMap = commonService.getJusoDetail("•Ï•Ï®°o¢¨¢Á", 1, 10);
+		Map<String, Object> reMap = commonService.getJusoDetail("ÎèÑÌèâÎ¶¨", 1, 10);
 		
 		logger.info("------------------------------------------");
 		System.out.println(reStr);
