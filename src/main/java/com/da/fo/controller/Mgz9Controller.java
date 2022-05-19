1c12681461bf11b90ca8da33ec86fdb9549c1185
mh.oh <blackjjoker@gmail.com>
2022-05-19 18:06:49 +0900

    mgz9, 헤더, pc css 등

---
+++ src/main/java/com/da/fo/controller/Mgz9Controller.java
@@ -0,0 +1,76 @@
+package com.da.fo.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.da.fo.service.Mgz9Service;
+
+@Controller
+public class Mgz9Controller {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private Mgz9Service mgz9Service;
+	
+	@RequestMapping("/insights")
+	public String openInsights() {
+		return "thymeleaf/fo/mgz/insights";
+	}
+	
+	@RequestMapping("/media")
+	public String openMedia() {
+		return "thymeleaf/fo/mgz/media";
+	}
+	
+	@RequestMapping("/exhibition")
+	public String openExhibition() {
+		return "thymeleaf/fo/mgz/exhibition";
+	}
+
+	/*
+	 * MGZ9 목록 조회
+	 * PARAM : 
+		#MGZ_TYP_CD
+			IST - INSIGHTS
+			MDA - MEDIA
+			EBI - EXHIBITION
+	 * RETURN : MGZ9 목록
+	 */
+	@RequestMapping("/selectMgz9List")
+	@ResponseBody
+	public ModelAndView selectMgz9List(@RequestParam(value = "mgzTypCd", required = true) String mgzTypCd) {
+		ModelAndView mv = new ModelAndView("jsonView");
+		logger.debug("############# mgzTypCd : " + mgzTypCd);
+		
+		List result = mgz9Service.selectMgz9List(mgzTypCd);
+		mv.addObject("result", result);
+		return mv;
+	}
+	
+	/*
+	 * MGZ9 상세 조회
+	 * PARAM : MGZ_SQ
+	 * RETURN : MGZ9 상세
+	 */
+	@RequestMapping("/selectMgz9Dtl")
+	@ResponseBody
+	public ModelAndView selectMgz9Dtl(@RequestParam(value = "mgzSq", required = true) String mgzSq) {
+		ModelAndView mv = new ModelAndView("jsonView");
+		logger.debug("############# mgzSq : " + mgzSq);
+		
+		Map<String, Object> result = mgz9Service.selectMgz9Dtl(mgzSq);
+		mv.addObject("result", result);
+		return mv;
+	}
+	
+}
---
+++ src/main/java/com/da/fo/service/Mgz9ServiceImpl.java
@@ -0,0 +1,41 @@
+package com.da.fo.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.da.fo.dao.Mgz9Dao;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class Mgz9ServiceImpl implements Mgz9Service{
+	
+	@Autowired
+	Mgz9Dao mgz9Dao;
+	
+	/*
+	 * MGZ9 목록 조회
+	 * PARAM : 
+		#MGZ_TYP_CD
+			IST - INSIGHTS
+			MDA - MEDIA
+			EBI - EXHIBITION
+	 * RETURN : MGZ9 목록
+	 */
+	public List selectMgz9List(String param) {
+		return mgz9Dao.selectMgz9List(param);
+	}
+	
+	/*
+	 * MGZ9 상세 조회
+	 * PARAM : MGZ_SQ
+	 * RETURN : MGZ9 상세
+	 */
+	public Map<String, Object> selectMgz9Dtl(String param){
+		return mgz9Dao.selectMgz9Dtl(param);
+	}
+}
