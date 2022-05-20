626ad5fa267c57fa0ccb0cfefd6c69909808601b
Jeong Sea Young <tjdud54z@naver.com>
2022-05-20 21:52:50 +0900

    게시판 변경사항 필드 수정

--- src/main/java/com/da/mapper/boardMapper.java
+++ src/main/java/com/da/mapper/boardMapper.java
@@ -4,7 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 public interface boardMapper {
-
+	
+	/**
+	 * 공지사항,FAQ DAO
+	 * 
+	 * **/
+	
 	//게시판 목록
 	public List boardList(Map<String, Object> param);
 	//게시판 등록
@@ -15,4 +20,21 @@ public interface boardMapper {
 	public void boardDelete(Map<String, Object> param);
 	//게시판 수정
 	public void boardUpdate(Map<String, Object> param);
+	
+	/**
+	 * 잡지 DAO
+	 * 
+	 * **/
+	
+	//잡지 목록
+	public List magazineList(Map<String, Object> param);
+	//잡지 등록
+	public void magazineInsert(Map<String, Object> param);
+	//잡지 상세
+	public List magazineDetail(Map<String, Object> param);
+	//잡지 삭제
+	public void magazineDelete(Map<String, Object> param);
+	//잡지 수정
+	public void magazineUpdate(Map<String, Object> param);
+	
 }
