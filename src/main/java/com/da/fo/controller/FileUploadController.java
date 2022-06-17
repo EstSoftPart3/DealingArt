ebf5cc4e8896ae4afe81a01f1446c09f65abfe5e
JungYoungWoo <est.part3@gmail.com>
2022-06-17 13:16:13 +0900

    css 수정 반영, 기타 오류 수

--- src/main/java/com/da/fo/controller/FileUploadController.java
+++ src/main/java/com/da/fo/controller/FileUploadController.java
@@ -28,7 +28,7 @@ public class FileUploadController {
 	private AwsS3Service awsS3Service;
 	
     @PostMapping("/upload")
-    public FileVo upload(@RequestPart(value = "file") MultipartFile multipartFile,@RequestPart(value = "url") String url) throws IOException {
+    public FileVo upload(@RequestPart(value = "file") MultipartFile multipartFile, @RequestPart(value = "url") String url) throws IOException {
     	System.out.println("######### file : " + multipartFile.getName());
         return awsS3Service.upload(multipartFile, url);
     }
